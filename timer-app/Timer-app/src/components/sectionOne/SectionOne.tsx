075287488d8095752592c3eedf3939d3b11d4676
the wiki <olusegundavids1@gmail.com>
2023-10-17 10:22:55 +0100

    On branch master

---
+++ timer-app/Timer-app/src/components/sectionOne/SectionOne.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./sectionOne.styles";
+
+const SectionOne = () => {
+  return (
+    <Container>
+        <div>
+            <h2>Online Alarm Clock</h2>
+            <p>Use a simple and convenient alarm clock online and for free</p>
+        </div>
+    </Container>
+  )
+}
+
+export default SectionOne;
---
+++ timer-app/Timer-app/src/components/sectionOne/sectionOne.styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+    background: #fff;
+    text-align: center;
+    padding-top: 2rem;
+    padding-bottom: 7rem;
+    margin-top: 1rem;
+
+    h2{
+        font-size: 2em;
+    }
+    
+`
